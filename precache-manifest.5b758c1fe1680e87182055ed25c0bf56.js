a96a88615942c3f7e143a235b4c200ea347c3daa
Muhammad Raza <muhammad.raza@intelex.com>
2019-12-05 19:15:52 -0500

    Updates

---
+++ precache-manifest.5b758c1fe1680e87182055ed25c0bf56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "703b26b1c2279343bd40c4b5badc1ca3",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "4da2bde374d53870250a",
+    "url": "/monster-rolodex/static/css/main.ddb86159.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monster-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "4da2bde374d53870250a",
+    "url": "/monster-rolodex/static/js/main.af8e12cc.chunk.js"
+  },
+  {
+    "revision": "ec2849d1eae557c66f4a",
+    "url": "/monster-rolodex/static/js/runtime-main.b891af59.js"
+  }
+]);
